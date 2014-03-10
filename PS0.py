42f9a0fed9769afd5c52c4718f38a6cfcda25176
jantsankhorloo <jantsankhorloo@gmail.com>
2014-03-10 12:59:53 +0800

    Create PS0.py
    It appears that they all cannot be executed at the same time. It they are done one at the time in above order, the code works as intended.

---
+++ PS0.py
@@ -0,0 +1,8 @@
+#Write a program that does the following in order:
+#1. Asks the user to enter his/her last name. 
+#2. Asks the user to enter his/her first name. 
+#3. Prints out the user’s first and last names in that order. 
+
+a = raw_input ('Enter your last name:')
+b = raw_input ('Enter your first name:')
+print a+' '+b
