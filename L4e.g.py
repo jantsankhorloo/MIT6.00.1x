d646759d8f439f2338920891e33f78e027b6d720
jantsankhorloo <jantsankhorloo@gmail.com>
2014-03-13 15:48:10 +0800

    Create L4e.g.py
    Some great examples from Lecture 4. 
    
    The fibonacci is example is based on how many number of rabbits there will be after x months.

---
+++ L4e.g.py
@@ -0,0 +1,9 @@
+def isPalindrome(s): #the input must be a string!
+    """Returns True if s is a Palindrome and False otherwise"""
+    if len(s) <= 1: return True
+    else: return s[0] == s[-1] and isPalindrome(s[1:-1])  
+    
+def fib(x): #on x == 36, it enters into a forever loop. control+c will be your friend
+    """Returns fibonacci of x, where x is non-negative int"""
+    if x == 0 or x == 1: return 1
+    else: return fib(x-1)+fib(x-2)
