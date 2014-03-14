8436dc8157e7af9c9843347550de8e8c1ef458c1
jantsankhorloo <jantsankhorloo@gmail.com>
2014-03-14 15:16:15 +0800

    Update L4e.g.py

--- L4e.g.py
+++ L4e.g.py
@@ -3,7 +3,7 @@ def isPalindrome(s): #the input must be a string!
     if len(s) <= 1: return True
     else: return s[0] == s[-1] and isPalindrome(s[1:-1])  
     
-def fib(x): #on x == 36, it enters into a forever loop. control+c will be your friend
+def fib(x): #on x == 36, it takes a while to calculate. Patience, young grasshopper!
     """Returns fibonacci of x, where x is non-negative int"""
     if x == 0 or x == 1: return 1
     else: return fib(x-1)+fib(x-2)
